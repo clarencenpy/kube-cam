75994c64b765bf58777593710a2ef1e1b8dfc608
Amanda <11187006+setttings@users.noreply.github.com>
2018-03-08 16:23:24 -0500

    Add tests for more sample application endpoints
    Notes
    - Example of a 403 request
    - And one that triggers internal service traffic

--- sample/traffic/generate_traffic.py
+++ sample/traffic/generate_traffic.py
@@ -5,12 +5,22 @@ def index(l):
     l.client.get('/')
 
 
+def error_403(l):
+        resp = l.client.post('/')
+
+
+def generate_interal_traffic(l):
+    l.client.get('/number')
+
+
 class GenerateTraffic(TaskSet):
-    tasks = { index: 2 }
+    tasks = { error_403 : 2, index: 5, generate_interal_traffic: 5}
 
 
     def start(self):
         index(self)
+        error_403(self)
+        generate_interal_traffic(self)
 
 
 class WebsiteUser(HttpLocust):
